edcb2962e50481e5c86bee35ad365bbc9cd93c49
wangkj <ne.wkj@qq.com>
2020-01-01 01:25:30 +0800

    show data in page record

--- miniprogram/pages/record/index.js
+++ miniprogram/pages/record/index.js
@@ -5,14 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    listData: [
+      { "code": "01", "text": "text1", "type": "type1" },
+      { "code": "02", "text": "text2", "type": "type2" },
+      { "code": "03", "text": "text3", "type": "type3" },
+      { "code": "04", "text": "text4", "type": "type4" },
+      { "code": "05", "text": "text5", "type": "type5" },
+      { "code": "06", "text": "text6", "type": "type6" },
+      { "code": "07", "text": "text7", "type": "type7" }
+    ]
+  },
 
+  getData: function () {
+    console.log('getData')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getData()
   },
 
   /**
