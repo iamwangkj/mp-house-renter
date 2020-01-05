4dded691fc0aadf4d06e5da6254aab4e301a4c1e
wangkj <ne.wkj@qq.com>
2020-01-05 13:49:17 +0800

    add user-avatar in page me

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -104,6 +104,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.cloud.callFunction({
+      name: 'login'
+    }).then((res) => {
+      console.log(res)
+      const { openid } = res.result
+    })
+
+
   },
 
   /**
